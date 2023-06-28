65ff23fe9b0712dc8b6fd70f38f38bc461f243b8
dilipg07 <dilip.calgry@gmail.com>
2023-06-28 20:19:32 +0530

    Created template for password manager

---
+++ main.py
@@ -0,0 +1,31 @@
+from tkinter import *
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50,pady=50)
+canvas = Canvas(width=200,height=200)
+lock = PhotoImage(file = "logo.png")
+canvas.create_image(100,100,image=lock)
+canvas.grid(row=0,column=1)
+# Labels
+website = Label(text="Website:")
+website.grid(row=1,column=0)
+email = Label(text="Username/Email:")
+email.grid(row=2,column=0)
+password = Label(text="Password:")
+password.grid(row=3,column=0)
+# Entry
+website_entry = Entry(width=43)
+website_entry.grid(row=1,column=1,columnspan=2)
+website_entry.focus()
+email_entry = Entry(width=43)
+email_entry.grid(row=2,column=1,columnspan=2)
+email_entry.insert(0,"dilip.calgry@gmail.com")
+password_entry = Entry(width=33)
+password_entry.grid(row=3,column=1)
+# Button
+generate_button =Button(text="Generate")
+generate_button.grid(row=3,column=2)
+add_button = Button(text="Add",width=36)
+add_button.grid(row=4,column=1,columnspan=2)
+
+window.mainloop()
